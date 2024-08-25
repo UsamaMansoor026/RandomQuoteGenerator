83803a37ed3f83decec9c7d22dddccc3901ab14f
Usama Mansoor <152704241+UsamaMansoor026@users.noreply.github.com>
2024-08-25 18:50:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const API = "https://dummyjson.com/quotes/random";
+const quote = document.querySelector(".container h3");
+const authorr = document.querySelector(".container p");
+const refreshBtn = document.querySelector(".container button");
+
+const fetchQuote = async () => {
+  await fetch("https://quotes-api-self.vercel.app/quote")
+    .then((response) => response.json())
+    .then((data) => {
+      quote.textContent = "'" + data.quote + "'";
+      authorr.textContent = data.author;
+    })
+    .catch((error) => {
+      // Handle any errors
+      console.error(error);
+    });
+};
+
+fetchQuote();
+
+refreshBtn.addEventListener("click", fetchQuote);
